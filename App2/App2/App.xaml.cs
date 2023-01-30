1a3ee7d3fc296533774d23a9f672cb546e505a29
Алексей Нефф <neff.alexey@gmail.com>
2023-01-30 18:45:06 +0200

    init

---
+++ App2/App2/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App2.Resources;
+using App2.Services;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            LocalizationResourceManager.Current.Init(AppResource.ResourceManager);
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App2/App2/ViewModels/AboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App2.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(() =>
+            {
+                LocalizationResourceManager.Current.CurrentCulture =
+                    LocalizationResourceManager.Current.CurrentCulture.TwoLetterISOLanguageName == "en" ?
+                        new CultureInfo("uk") :
+                        new CultureInfo("en");
+            });
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
